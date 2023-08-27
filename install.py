ad0efcbf4f457cb2d20dcd0ee663d4789f996473
TheDuggy <gkollegger5@outlook.com>
2023-08-27 13:38:16 +0200

    Improved menu.

--- install.py
+++ install.py
@@ -24,19 +24,34 @@ class PlatformSupport(Enum):
 
 class Menu():
     def __init__(self, options: list) -> None:
-
         self.options = options
         self.selection = self.options[0]
         self.__cursor_pos = len(self.options) - 1
+        self.__first_pressed = False
 
+        # hide cursor
+        print("\033[?25l", end="")
+
+        # print available options
         for s in options:
-            print(f"\t{s}")
+            print(f"    {s}")
             self.__cursor_pos -= 1 
         print(Cursor.UP(), end="")
         self.__cursor_pos += 1
+        self.options.reverse()
+        # start keyboard-listener
         listen_keyboard(on_press=self.__on_press, until="enter", delay_other_chars=0.01, delay_second_char=0.01) 
+
+        # remove hover-effect after enter is pressed
+        self.__clear_line()
+        print(f"{' ' * 4}{self.selection}")
         print(Cursor.DOWN(len(options) - 1))
 
+        # show cursor
+        print("\033[?25h", end="")
+        
+    def __clear_line(self) -> None:
+        print("\033[2K\033[G", end="")
 
     def __on_press(self, key) -> None:
         if key == "up":
@@ -45,31 +60,41 @@ class Menu():
             self.__hover(-1)
 
     def __on_select(self, option) -> str:
-        return Fore.YELLOW + f"\t  > {option}" + Fore.RESET
+        return Fore.YELLOW + f"{' ' * 4}  > {option}" + Fore.RESET
 
     def __hover(self, move: int) -> None:
-        
-        print("\033[2K\033[G", end="")
-        print(Fore.RESET + "\t" + self.selection, end="\r")
 
-        self.selection = self.options[((self.options.index(self.selection) + move) + len(self.options)) % len(self.options)]
+        if self.__first_pressed:
 
-        if self.__cursor_pos == len(self.options) - 1 and move > 0:
-            print(Cursor.DOWN(len(self.options) - 2))
-            self.__cursor_pos = 0
-        elif self.__cursor_pos == 0 and move < 0:
-            print(Cursor.UP(len(self.options)))
-            self.__cursor_pos = len(self.options) - 1
-        else:
-            if move < 0:
-                print(Cursor.DOWN(abs(move)), end = "")
-                self.__cursor_pos -= 1
+            # remove hover-effect from previous selection
+            self.__clear_line()
+            print(Fore.RESET + f"{' ' * 4}{self.selection}", end="\r")
+
+            self.selection = self.options[((self.options.index(self.selection) + move) + len(self.options)) % len(self.options)]
+
+            if self.__cursor_pos == len(self.options) - 1 and move > 0: # if you are at the menu-top, go to the bottum when <up> is pressed
+                print(Cursor.DOWN(len(self.options) - 2))
+                self.__cursor_pos = 0
+            elif self.__cursor_pos == 0 and move < 0: # if you are at the menu-bottum, go to the top when <down> is pressed
+                print(Cursor.UP(len(self.options)))
+                self.__cursor_pos = len(self.options) - 1
             else:
-                print(Cursor.UP(abs(move)), end = "")
-                self.__cursor_pos += 1
-    
-        print("\033[2K\033[G", end="")
+                if move < 0:
+                    print(Cursor.DOWN(), end = "") # in case of normal movement, just go up or down
+                    self.__cursor_pos -= 1
+                else:
+                    print(Cursor.UP(), end = "")
+                    self.__cursor_pos += 1
+            
+        else:
+            print(Cursor.UP(len(self.options) - 1), end="")
+            self.__cursor_pos = len(self.options) - 1
+            self.__first_pressed = True
+
+        # print the hover-effect
+        self.__clear_line()
         print(self.__on_select(self.selection), end="\r")
+        
 
 class Configuration():
     def __init__(self, id: str, platform: PlatformSupport, conf_version: int) -> None:
@@ -95,7 +120,7 @@ def main():
     init()
 
     print(LOGO, end = " ")
-    print(Fore.YELLOW + "conf-everything " + Fore.BLACK + Back.YELLOW + "v" + VERSION + Fore.RESET + Back.RESET)
+    print(Fore.YELLOW + "conf-everything " + Fore.BLACK + Back.YELLOW + "v" + VERSION + Fore.RESET + Back.RESET, end="\n\n")
 
     menu = Menu([
         "Test1",
